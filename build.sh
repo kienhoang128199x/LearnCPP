55b6bfe3f8d78db1f8c5b1cf9f01b7fa42b3a313
kienhoang128199x <kienhoang128199x@gmail.com>
2024-05-16 00:34:41 +0700

    Add Makefile

--- build.sh
+++
@@ -1,6 +0,0 @@
-echo "Build ecallApp.cpp start"
-g++ -Wall -pthread -o ecallApp.exe ecallApp.cpp
-echo "Build ecallApp.cpp finish"
-echo "Build main.cpp start"
-g++ -Wall -pthread -o main.exe main.cpp lib.cpp
-echo "Build main.cpp finish"
